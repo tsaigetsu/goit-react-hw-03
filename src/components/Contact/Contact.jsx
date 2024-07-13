7649fbd717b27b10f479cf20313b8e2aebade814
Vladyslav Aka Tsaigetsu <callmepempai@gmail.com>
2024-07-13 19:58:20 +0200

    error fix 2

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-const ContactForm = ({conact, deleteContact}) => {
+const ContactForm = ({contact, deleteContact}) => {
   return (
     <div>
-      <h2>{ontact.name}</h2>
+      <h2>{contact.name}</h2>
       <p>{contact.number}</p>
       <button onClick={() => deleteContact(contact.id)}>Delete</button>
     </div>
